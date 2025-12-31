68fb7eebc8970f080f94eb124804a327a873d10d
Purvansh <purvansh.23bcs10169@ms.sst.scaler.com>
2025-12-31 23:38:06 +0530

    feat: Add hover pop-out effects to components and configure animated cursor interactions.

--- src/App.js
+++ src/App.js
@@ -89,12 +89,27 @@ function App() {
         color="#F94892"
         innerSize={8}
         outerSize={0}
-        innerScale={1}
+        innerScale={1.2}
         outerScale={1}
         outerAlpha={0}
+        hasBlendMode={false}
         innerStyle={{
           backgroundColor: "#F94892",
+          zIndex: 99999,
         }}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
       />
 
       {/* Main Content */}
